2bed7176f911bb729e913dde408434fa7a024153
cnddy <86910496+cnddy@users.noreply.github.com>
2022-03-26 21:29:18 +0800

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -4,6 +4,6 @@
  mount -t tmpfs rootfs -o size=100% $DIR
  
 cd $DIR 
-wget -qO- 'https://gh.api.99988866.xyz/https://github.com/cnddy/ostool/releases/download/1.0/alpine-rootfs.cpio.gz' | gunzip -c | cpio --verbose -id
+wget -qO- 'https://github.com/cnddy/ostool/releases/download/1.0/alpine-rootfs.cpio.gz' | gunzip -c | cpio --verbose -id
  
 systemctl --no-block switch-root $DIR $INIT
