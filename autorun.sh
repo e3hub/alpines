9f0ef67e01c30d22d09bcb72c1b3952d534627e4
Bicsac <86910496+Bicsac@users.noreply.github.com>
2022-08-31 04:30:15 +0800

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -4,6 +4,6 @@
  mount -t tmpfs rootfs -o size=100% $DIR
  
 cd $DIR 
-wget -qO- '' | gunzip -c | cpio --verbose -id
+wget -qO- 'https://github.com/Bicsac/NetsDD/releases/download/1.0/alpine-rootfs.cpio.gz' | gunzip -c | cpio --verbose -id
  
 systemctl --no-block switch-root $DIR $INIT
