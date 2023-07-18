c3b7433132b75428eae825293884f8b34e81a185
e3hub <86910496+e3hub@users.noreply.github.com>
2023-07-18 19:06:39 +0800

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -5,6 +5,6 @@ setenforce 0 2>&-
  mount -t tmpfs rootfs -o size=100% $DIR
  
 cd $DIR 
-wget --no-check-certificate -qO- 'https://github.com/Janxget/Eiroot/releases/download/11.0/minirootfs.tar.gz' | tar -xz
+wget --no-check-certificate -qO- 'https://github.com/e3hub/alpines/releases/download/11.0/minirootfs.tar.gz' | tar -xz
  modprobe -a zram btrfs ext4 xfs vfat
 systemctl  switch-root $DIR $INIT
